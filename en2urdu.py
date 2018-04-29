8ea973b4eeed42d5bd2697442a5a64fdabc8deaa
Abid H Mujtaba <abid.naqvi83@gmail.com>
2018-04-29 23:37:05 +0500

    Add lower-case keyboard mappings eng to urdu

---
+++ en2urdu.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python3
+
+"""
+	This is a utility for converting english characters to their equivalent urdu characters as defined by the CRULP Urdu Keyboard Layout v1.1 (and vice versa).
+"""
+
+# Define the map between english and urdu characters (the latter defined using their Unicode code-points)
+
+e2u = {
+	1: f1,
+	2: f2,
+	3: f3,
+	4: f4,
+	5: f5,
+	6: f6,
+	7: f7,
+	8: f8,
+	9: f9,
+	10: f0,
+	q: 42,
+	a: 27,
+	z: 32,
+	w: 48,
+	s: 33,
+	x: 34,
+	e: 39,
+	d: 2f,
+	c: 86,
+	r: 31,
+	f: 41,
+	v: 37,
+	t: 2a,
+	g: af,
+	b: 28,
+	y: d2,
+	h: 2d,
+	n: 46,
+	u: 21,
+	j: 2c,
+	m: 45,
+	i: cc,
+	k: a9,
+	,: 0c,
+	o: c1,
+	l: 44,
+	.: d4,
+	p: 7e,
+	;: 1b,
+}
+
+def main():
+
+	print("Hello, World")
+
+
+if __name__ == '__main__':
+	
+	main()
